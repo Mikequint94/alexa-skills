085ce0eb6631cbf7f62d62833c3f0bd29e0a446c
Michael Quint <mjq8@cornell.edu>
2017-12-26 19:27:38 -0500

    added fragment and dictionary

--- ghost/index.js
+++ ghost/index.js
@@ -1,4 +1,9 @@
 const Alexa = require('alexa-sdk');
+let fs = require("fs");
+  const text = fs.readFileSync('dictionary.txt', "utf-8");
+  let dictionary = text.split("\n");
+  dictionary.splice(-1);
+  let fragment = "";
 
 exports.handler = function(event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
@@ -27,6 +32,20 @@ const handlers = {
     },
     'letterIntent' : function() {
         let letter = this.event.request.intent.slots.letter.value || "A";
-        this.emit(':tell', `you picked ${letter}`);
+        fragment += letter;
+        let speechOutput = `You picked ${letter}. The total fragment is ${fragment}`;
+       
+        if (winRound()) {
+           speechOutput += `you win this round`;
+        } else {
+           speechOutput += `the round continues`;
+        }
+        this.emit(':tell', speechOutput);
+      
+
     }
 };
+
+let winRound =function() {
+    return dictionary.includes(fragment);
+}
